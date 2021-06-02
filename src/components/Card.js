c6c5a3dd79dbe7ebd8fae5abf33aabc179623f2e
Oksana Kostenko <kseniavlad31@gmail.com>
2021-06-02 14:11:29 +0100

    adds classes to Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,19 @@
 const Card = ({ vacancy }) => {
   return (
     <div className="card">
-      <img src={vacancy.logo} alt={`${vacancy.company} logo`} />
-      <h4>{vacancy.company}</h4>
-      <h3>{vacancy.position}</h3>
-      <ul>
-        <li>{vacancy.postedAt}</li>
-        <li>{vacancy.contract}</li>
-        <li>{vacancy.location}</li>
-      </ul>
-      <ul>
+      <div className="card-logo">
+        <img src={vacancy.logo} alt={`${vacancy.company} logo`} />
+      </div>
+      <div className="card-vacancy-info">
+        <h4>{vacancy.company}</h4>
+        <h3>{vacancy.position}</h3>
+        <ul>
+          <li>{vacancy.postedAt}</li>
+          <li>{vacancy.contract}</li>
+          <li>{vacancy.location}</li>
+        </ul>
+      </div>
+      <ul className="card-vacancy-requirements">
         <li>{vacancy.role}</li>
         {vacancy.languages.map((language, index) => {
           return <li key={index}>{language}</li>;
